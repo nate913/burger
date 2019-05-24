5fafb64f6b225ea794676efd9e207cb9632a2844
nate pearson <nate913@gmail.com>
2019-05-24 12:54:14 -0500

    added files

---
+++ config/connection.js
@@ -0,0 +1,25 @@
+// Connect Node to MySQL.
+var mysql = require("mysql");
+
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        port: "",
+        host: "localhost",
+        user: "root",
+        password: "deadpool1",
+        database: "burgers_db"
+    });
+};
+
+connection.connect(function(err) {
+    if (err) {
+        console.error("error connecting: " + err.stack);
+        return;
+    }
+    console.log("connected as id " + connection.threadId);
+});
+
+// Export the connection.
+module.exports = connection;
